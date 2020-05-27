ed9cba81b751067957ce22adc9ed4984107cabb8
Dan Sweetman <sweetman3895@gmail.com>
2020-05-27 10:51:29 -0700

    im a real engineer now lads

--- js/konami.js
+++ js/konami.js
@@ -1,4 +1,4 @@
-var allowedKeys = {
+const allowedKeys = {
     37: 'left',
     38: 'up',
     39: 'right',
@@ -6,13 +6,13 @@ var allowedKeys = {
     65: 'a',
     66: 'b'
 };
-var memes = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://youtu.be/L_jWHffIx5E?t=35", "https://www.youtube.com/watch?v=jofNR_WkoCE" , "https://www.youtube.com/watch?v=ZZ5LpwO-An4", "https://www.youtube.com/watch?v=U9t-slLl30E","https://www.youtube.com/watch?v=0tdyU_gW6WE&t=99s"];
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-var konamiCodePosition = 0;
+const memes = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://youtu.be/L_jWHffIx5E?t=35", "https://www.youtube.com/watch?v=jofNR_WkoCE" , "https://www.youtube.com/watch?v=ZZ5LpwO-An4", "https://www.youtube.com/watch?v=U9t-slLl30E","https://www.youtube.com/watch?v=0tdyU_gW6WE&t=99s"];
+const konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+let konamiCodePosition = 0;
 
 document.addEventListener('keydown', function(e) {
-    var key = allowedKeys[e.keyCode];
-    var requiredKey = konamiCode[konamiCodePosition];
+    const key = allowedKeys[e.keyCode];
+    const requiredKey = konamiCode[konamiCodePosition];
 
     if (key === requiredKey) {
         konamiCodePosition++;
--- js/titles.js
+++ js/titles.js
@@ -1,12 +1,12 @@
-var update = ["Software Engineer", "Swift Enthusiast", "iOS Dev", "WatchKit Dev", "React JS Dev", "I make the magic happen!"];
-var last = 0;
-function change(last, next) {
-    if (next !== last) {
-        self.last = next;
-        document.getElementById("ran-update").innerHTML = update[next];
+const update = ["Software Engineer", "Swift Enthusiast", "iOS Dev", "WatchKit Dev", "React JS Dev", "I make the magic happen!"];
+let currentTitle = 0;
+function changeTitle(lastTitle, nextTitle) {
+    if (nextTitle !== lastTitle) {
+        currentTitle = nextTitle;
+        document.getElementById("ran-update").innerHTML = update[nextTitle];
     } else {
-        change(last, Math.floor(Math.random() * update.length));
+        changeTitle(lastTitle, Math.floor(Math.random() * update.length));
     }
 }
 
-setInterval(function(){change(last, Math.floor(Math.random() * update.length))}, 2500);
+setInterval(function(){changeTitle(currentTitle, Math.floor(Math.random() * update.length))}, 2500);
