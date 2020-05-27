8837c68d3f9a9f890a256f981b436157ed6ce535
Dan Sweetman <sweetman3895@gmail.com>
2020-05-26 22:15:26 -0700

    Oooops part 2

--- js/titles.js
+++ js/titles.js
@@ -3,10 +3,10 @@ var last = 0;
 function change(last, next) {
     if (next !== last) {
         self.last = next;
-        return document.getElementById("ran-update").innerHTML = update[next];
+        document.getElementById("ran-update").innerHTML = update[next];
     } else {
-        return self.change(last, Math.floor(Math.random() * update.length));
+        change(last, Math.floor(Math.random() * update.length));
     }
 }
 
-setInterval(function () { change(last, Math.floor(Math.random() * update.length)) }, 2500);
+setInterval(function(){change(last, Math.floor(Math.random() * update.length))}, 2500);
